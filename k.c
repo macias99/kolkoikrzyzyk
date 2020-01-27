70e815da64533d9f2a13c89be650dc83823bbfb8
Karol Zedlewski <vindrohec@gmail.com>
2020-01-27 14:15:14 +0100

    commit

---
+++ k.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+enum ustalPole { K_Pole, W_Pole, Puste_Pole};
+
+#define K_wielkosc 3
+#define W_wielkosc 3
+
+ustalPole plansza[K_wielkosc][W_wielkosc];
+
+int main()
+{
+
+
+
+    return 0;
+}
